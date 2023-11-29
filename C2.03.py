8d0426028d000d94ed0b2e708ce1db5a828633c3
Phạm Minh Ngọc Hà <73472735+phamminhngocha@users.noreply.github.com>
2023-11-29 21:15:45 +0700

    Add files via upload

---
+++ C2.03.py
@@ -0,0 +1,7 @@
+#Câu 2.3
+# Nhập nhiệt độ Celsius từ người dùng
+nhiet_do_C = float(input("Nhập nhiệt độ Celsius: "))
+# Tính nhiệt độ Fahrenheit
+nhiet_do_F = nhiet_do_C * 9/5 + 32
+# In ra màn hình
+print(f"Nhiệt độ Fahrenheit tương ứng là: {nhiet_do_F} °F")
---
+++ C2.04.py
@@ -0,0 +1,7 @@
+#Câu 2.4
+# Nhập nhiệt độ Fahrenheit từ người dùng
+nhiet_do_F = float(input("Nhập nhiệt độ Fahrenheit: "))
+# Tính nhiệt độ Celsius
+nhiet_do_C = (nhiet_do_F - 32) * 5/9
+# In ra màn hình
+print(f"Nhiệt độ Celsius tương ứng là: {nhiet_do_C} °C")
